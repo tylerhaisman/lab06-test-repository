820622e4623ce33d0f431a768c7361a70b8f0e58
Tyler Haisman <tyler@haisman.net>
2022-03-11 14:30:05 -0600

    my second commit that allows two numbers to be added or subtracted

--- Lab06.java
+++ Lab06.java
@@ -5,16 +5,23 @@ public class addTwoNumbers {
     public static void main(String[] args) {
         //initializing scanner
         Scanner scanner = new Scanner(System.in);
+        int result = 0
         //prompting the user for two numbers and storing them into variables "num1" and "num2"
-        System.out.println("Which numbers would you like to add?");
         System.out.println("Input first number");
         int num1 = scanner.nextInt();
         System.out.println("Input second number");
         int num2 = scanner.nextInt();
-
-        //storing the resulting sum of the two variables into "result"
-        int result = num1 + num2;
-
+        
+        System.out.println("Type 1 for addition and 2 for subtraction");
+        if(scanner.nextInt() == 1){
+            //storing the resulting sum of the two variables into "result"
+        result = num1 + num2;
+        }
+        else{
+            //storing the resulting difference of the two variables into "result"
+        result = num1 - num2;
+        }
+     
         //printing the result of the two variables
         System.out.println(result);
     }
